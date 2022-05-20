e4ed3ef9f196af883c799f35b034fcf18384646a
glodeanu_dan <dannu5glodeannu@gmail.com>
2022-05-21 02:43:40 +0300

    add constants

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -1,3 +1,6 @@
+import { useEffect, useState, } from 'react';
+import axios from "axios";
+import { API_URL, } from "../../constants";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,10 +13,40 @@ import Header from '../components/Header';
 import Card from '../components/Card';
 
 function Requests() {
+  const [user, updateUser] = useState();
+  const [requests, updateRequests] = useState();
 
   const status = 'closed';
   const productName = 'iaurt cu fructe';
   const quantity = 10;
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      axios.get(API_URL + "/auth/user_data",
+          {
+            headers: {
+              token: localStorage.getItem("token")
+          },
+      })
+      .then(res => {
+        updateUser(res.data);
+        console.log(res.data)
+      })
+      .catch(err => console.log(err));
+
+      axios.get(API_URL + "/request",
+          {
+            headers: {
+              token: localStorage.getItem("token")
+          },
+      })
+      .then(res => {
+        updateRequests(res.data);
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+    }
+  }, [])
 
   const obj = {
     status: 'closed',
